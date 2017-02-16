d04f9791d465b0c805f9d6e60c9c41a8337a9fd8
ismaelc <chrisi@13mbp-11334.gateway.pace.com>
2017-02-16 01:13:07 -0800

    test

---
+++ messages/google.js
@@ -0,0 +1,192 @@
+var google = require('googleapis');
+var OAuth2 = google.auth.OAuth2;
+var async = require('async');
+var htmlToText = require('html-to-text');
+
+var client_id = process.env['GOOGLE_CLIENT_ID'];
+var client_secret = process.env['GOOGLE_CLIENT_SECRET'];
+var redirect_url = process.env['AUTH_REDIRECT_URL']; //'http://localhost:5000/oauth2callback';
+
+var oauth2Client = new OAuth2(
+  client_id,
+  client_secret,
+  redirect_url
+);
+
+//console.log(generateAuthURL());
+
+function generateAuthURL() {
+
+  // generate a url that asks permissions for Google+ and Google Calendar scopes
+  var scopes = [
+    'https://www.googleapis.com/auth/plus.me',
+    'https://www.googleapis.com/auth/calendar',
+    'https://www.googleapis.com/auth/gmail.readonly'
+  ];
+
+  var url = oauth2Client.generateAuthUrl({
+    // 'online' (default) or 'offline' (gets refresh_token)
+    access_type: 'offline',
+    // If you only need one scope you can pass it as string
+    scope: scopes
+  });
+
+  console.log(url);
+  return url;
+}
+
+function retrieveAccessToken(code, callback) {
+  oauth2Client.getToken(code, function(err, tokens) {
+    // Now tokens contains an access_token and an optional refresh_token. Save them.
+    if (!err) {
+      oauth2Client.setCredentials(tokens);
+      console.log('Tokens: ' + JSON.stringify(tokens));
+      callback(null, oauth2Client);
+    } else callback(err, null);
+  });
+}
+
+function listMessages(options, callback) {
+  var gmail = google.gmail('v1');
+  gmail.users.messages.list(options, function(err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      callback(err, 'The API returned an error: ' + response);
+    } else {
+      var messages = response.messages;
+      //var output = [];
+      console.log('Type: ' + JSON.stringify(response));
+      if (messages.length == 0) {
+        //console.log('No messages found.');
+      } else {
+
+        /*
+        console.log('Messages:');
+        for (var i = 0; i < messages.length; i++) {
+          var message = messages[i];
+          console.log(message);
+          output.push(message);
+        }
+        */
+
+        async.mapLimit(messages, 10, function(message, _callback) {
+          //_callback(null, message['id']);
+          getMessage(options['auth'], 'me', message['id'], function(err, data) {
+
+            if (err) _callback(err, data);
+            else {
+
+              //console.log('Payload: ' + JSON.stringify(data.payload));
+
+              var type = data.payload.mimeType.split('/');
+              var parts = data.payload.parts;
+              if(type[0] == "multipart" && type[1] == "related") parts = data.payload.parts[0].parts;
+
+
+              //console.log('Payload: ' + JSON.stringify(parts));
+
+              var html_parts = parts.filter(function(part) {
+                return part.mimeType == 'text/html';
+              });
+
+              //console.log("Part: " + JSON.stringify(html_parts));
+
+              if (html_parts.length > 0) {
+
+                var date = extractField(data, "Date");
+                var subject = extractField(data, "Subject")
+
+                var b64string = html_parts[0].body.data.replace(/-/g, '+').replace(/_/g, '/');
+                var htmlfromText = htmlToText.fromString(
+                    new Buffer(b64string, 'base64')
+                    .toString('ascii')
+                    .replace(/(\r\n|\n|\r)/gm, " "), {
+                      ignoreHref: true,
+                      ignoreImage: true
+                    }
+                  )
+                  .replace(/(\r\n|\n|\r)/gm, " "); //.replace(/\r?\n|\r/g, " "))
+
+                var mail = {
+                  'id': data['id'],
+                  'date': extractField(data, "Date"),
+                  'snippet': data['snippet'],
+                  'subject': extractField(data, "Subject"),
+                  'body': htmlfromText
+                }
+
+                _callback(null, mail);
+
+                //_callback(null, html_parts[0]);
+                //_callback(null, subject);
+
+              } else _callback(null, null);
+
+              //_callback(null, data.payload);
+            }
+          })
+        }, function(err, results) {
+          if (err) callback(err, results);
+          else callback(null, results);
+        });
+
+      }
+      //callback(null, output);
+    }
+  });
+}
+
+function getMessage(auth, userId, messageId, callback) {
+
+  var options = {
+    'auth': auth,
+    'userId': userId,
+    'id': messageId
+  }
+
+  var gmail = google.gmail('v1');
+  gmail.users.messages.get(options, function(err, response) {
+    if (err) callback(err, response);
+    else callback(null, response);
+  });
+}
+
+var extractField = function(json, fieldName) {
+  return json.payload.headers.filter(function(header) {
+    return header.name === fieldName;
+  })[0].value;
+};
+
+function listLabels(auth, callback) {
+  var gmail = google.gmail('v1');
+  gmail.users.labels.list({
+    auth: auth,
+    userId: 'me',
+  }, function(err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      callback(err, 'The API returned an error: ' + response);
+    }
+    var labels = response.labels;
+    var output;
+    if (labels.length == 0) {
+      //console.log('No labels found.');
+      output = 'No labels found';
+    } else {
+      //console.log('Labels:');
+      output = 'Labels:';
+      for (var i = 0; i < labels.length; i++) {
+        var label = labels[i];
+        //console.log('- %s', label.name);
+        output += '- ' + label.name;
+      }
+    }
+    console.log(output);
+    callback(null, output);
+  });
+}
+
+exports.generateAuthURL = generateAuthURL;
+exports.retrieveAccessToken = retrieveAccessToken;
+exports.listMessages = listMessages;
+exports.listLabels = listLabels;
--- messages/index.js
+++ messages/index.js
@@ -1,6 +1,8 @@
 "use strict";
 var builder = require("botbuilder");
 var botbuilder_azure = require("botbuilder-azure");
+var luis = require('./luis_stub.js');
+var google = require('./google.js');
 
 var useEmulator = (process.env.NODE_ENV == 'development');
 
@@ -13,8 +15,34 @@ var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure
 
 var bot = new builder.UniversalBot(connector);
 
-bot.dialog('/', function (session) {
-    session.send('You said ' + session.message.text);
+bot.dialog('/', function(session) {
+    //session.send('You said ' + session.message.text);
+
+    luis.getIntent(session.message.text, function(err, response) {
+        var intent = response.topScoringIntent.intent;
+        var entities = response.entities;
+
+        switch (intent) {
+            case 'Login':
+                console.log('... to Login intent');
+                //var stateObjectBuffer = new Buffer(JSON.stringify(stateObject)).toString('base64');
+                var card = new builder.SigninCard(session)
+                    .text('Gmail (Google) Sign-in')
+                    .button('Sign-in', google.generateAuthURL());
+
+                var msg = new builder.Message(session).addAttachment(card);
+
+                // If message was typed in a group/channel, turn it into PM as this is login
+                if ((typeof session.message.address.conversation.name !== 'undefined') && (session.message.address.channelId != 'webchat')) delete session.message.address.conversation;
+
+                session.send(msg);
+                break;
+            default:
+                //if ((typeof session.message.address.conversation.name !== 'undefined') && (session.message.address.channelId != 'webchat')) delete session.message.address.conversation;
+                session.send('Hey')
+                break;
+        }
+    });
 });
 
 if (useEmulator) {
@@ -23,7 +51,9 @@ if (useEmulator) {
     server.listen(3978, function() {
         console.log('test bot endpont at http://localhost:3978/api/messages');
     });
-    server.post('/api/messages', connector.listen());    
+    server.post('/api/messages', connector.listen());
 } else {
-    module.exports = { default: connector.listen() }
+    module.exports = {
+        default: connector.listen()
+    }
 }
