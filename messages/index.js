f8fc30afa0ba6a52de8e4ef5c1405e895a5124de
ismaelc <chrisi@13MBP-11334.mshome.net>
2017-03-08 08:51:05 -0800

    test

--- messages/index.js
+++ messages/index.js
@@ -259,7 +259,16 @@ if (useEmulator) {
     });
     server.post('/api/messages', connector.listen());
 } else {
+    /*
     module.exports = {
         default: connector.listen()
     }
+    */
+    var listener = connector.listen();
+    var withLogging = function(context, req) {
+        console.log = context.log;
+        listener(context, req);
+    }
+
+    module.exports = { default: withLogging }    
 }
